15f86f2dd6ad2af421d82d680fc0ce2d93fefc56
Sadiqur Rahman <sadiqurrahman9m05@gmail.com>
2022-11-01 09:56:49 +0600

    fix border bug

--- components/Message.jsx
+++ components/Message.jsx
@@ -1,6 +1,6 @@
 export default function Message({ children, username, description }) {
   return (
-    <div className="bg-white p-2 sm:p-8 border-b-2 rounded-lg">
+    <div className="bg-white p-2 sm:p-8 shadow-lg rounded-lg">
       <h2 className="font-medium text-lg sm:text-2xl">{username}</h2>
       <div className="py-4">
         <p className="font-normal text-md">{description}</p>
